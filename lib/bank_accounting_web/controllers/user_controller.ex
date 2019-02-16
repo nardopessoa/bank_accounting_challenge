2c29df5a19d1f6c2474535094658369e42df6063
Leonardo Pessoa <nardopessoa@gmail.com>
2019-02-15 23:49:05 -0200

    Remove funções criadas pelo generators do phoenix porém não utilizadas

--- lib/bank_accounting_web/controllers/user_controller.ex
+++ lib/bank_accounting_web/controllers/user_controller.ex
@@ -7,11 +7,6 @@ defmodule BankAccountingWeb.UserController do
 
   action_fallback BankAccountingWeb.FallbackController
 
-  def index(conn, _params) do
-    users = Auth.list_users()
-    render(conn, "index.json", users: users)
-  end
-
   def create(conn, %{"user" => user_params}) do
     with {:ok, %User{} = user} <- Auth.create_user(user_params),
          {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
@@ -24,22 +19,6 @@ defmodule BankAccountingWeb.UserController do
   def show(conn, _params) do
     user = Guardian.Plug.current_resource(conn)
     render(conn, "show.json", user: user)
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Auth.get_user!(id)
-
-    with {:ok, %User{} = user} <- Auth.update_user(user, user_params) do
-      render(conn, "show.json", user: user)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    user = Auth.get_user!(id)
-
-    with {:ok, %User{}} <- Auth.delete_user(user) do
-      send_resp(conn, :no_content, "")
-    end
   end
 
   def login(conn, %{"username" => username, "password" => password}) do
--- lib/bank_accounting_web/views/user_view.ex
+++ lib/bank_accounting_web/views/user_view.ex
@@ -1,13 +1,8 @@
 defmodule BankAccountingWeb.UserView do
   use BankAccountingWeb, :view
-  alias BankAccountingWeb.UserView
-
-  def render("index.json", %{users: users}) do
-    %{data: render_many(users, UserView, "user.json")}
-  end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, UserView, "user.json")}
+    %{data: render_one(user, __MODULE__, "user.json")}
   end
 
   def render("user.json", %{user: user}) do
