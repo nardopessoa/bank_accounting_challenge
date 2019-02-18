665a5eb355b4db6af186d5506af4d2bc399be8a9
Leonardo Pessoa <nardopessoa@gmail.com>
2019-02-18 02:23:39 -0300

    Atualiza versões de dependências do projeto

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BankAccounting.MixProject do
     [
       app: :bank_accounting,
       version: "0.0.1",
-      elixir: "~> 1.8.1",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -36,16 +36,16 @@ defmodule BankAccounting.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.1"},
-      {:phoenix_pubsub, "~> 1.1.1"},
-      {:phoenix_ecto, "~> 4.0.0"},
-      {:ecto_sql, "~> 3.0.5"},
-      {:postgrex, ">= 0.14.1"},
-      {:gettext, "~> 0.16.1"},
-      {:jason, "~> 1.1.2"},
-      {:plug_cowboy, "~> 2.0.1"},
-      {:bcrypt_elixir, "~> 2.0.0"},
-      {:guardian, "~> 1.2.1"}
+      {:phoenix, "~> 1.4"},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, "~> 0.14"},
+      {:gettext, "~> 0.16"},
+      {:jason, "~> 1.1"},
+      {:plug_cowboy, "~> 2.0"},
+      {:bcrypt_elixir, "~> 2.0"},
+      {:guardian, "~> 1.2"}
     ]
   end
 
